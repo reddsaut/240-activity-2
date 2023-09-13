4e52297234ce010856051df3eef0eecabd443934
osauter <osauter@macalester.edu>
2023-09-13 13:17:18 -0500

    moved-to-new-repo

---
+++ activity-2-new-redding/activity2.c
@@ -0,0 +1,92 @@
+//reddsaut
+#include <stdio.h>        // printf
+#include <assert.h>       // assert
+#include <limits.h>       // named constants for min, max of C integer data types
+#include <math.h>
+
+
+float sales_example(float item_cost, unsigned short num_items, float tax);
+
+void loop_example();
+
+// TODO: (Task 2) Put declaration of loop_example here
+
+void utype_sizes();
+
+// TODO: (Task 4) Put declaration of stype_sizes here
+
+
+
+int main() {
+    // TASK 1: sales example
+    /*float total1;
+    total1 = sales_example(3.94, 4, 0.06);
+    printf("Total1 = $%.2f\n", total1);5ts
+    // TODO: Test on multiple calls, then add assert statements here
+    assert(fabs(total1 - 16.71) < 0.01);  */
+
+    // TASK 2: loop experiment
+    loop_example();
+
+    // TASK 3: unsigned integer types, sizes, and maximums
+    utype_sizes();
+
+    // TASK 4: signed integer types, sizes, mins and maxs
+    // TODO: place function call here
+
+    return 0;
+}
+
+
+/** TODO TASK 1:
+    * Complete the function below to implement the simple sales example
+    */
+float sales_example(float item_cost, unsigned short num_items, float tax) {
+    // TODO: Complete this definition (removing the default return)
+    float cost_before_tax = item_cost * num_items;
+
+    if(tax == 0.0) {
+        printf("No sales tax on this item");
+    }
+    return cost_before_tax * (1 + tax);
+}
+
+/** TODO TASK 2:
+    * Create your loop_example function here
+    */
+
+void loop_example() {
+    printf("for loop\n");
+    for(int i=10; i<=25; i+=5) {
+        printf(i + " " + i*i);
+    }
+    printf("\n");
+    int x = 10;
+    printf("while loop\n");
+    while(x<=25) {
+        x+=5;
+        printf(x + " " + x*x);
+    }
+    printf("\n");
+    x=10;
+    printf("do loop\n");
+    do {
+        x+=5;
+        printf(x + " " + x*x);
+    } while(x<25);
+}
+
+/** TODO TASK 3:
+    * Add print statements to print the max values and sizes of each unsigned
+    * integer type (short, int, long), based on the example of unsigned char.
+    */
+void utype_sizes() {
+	printf("unsigned char | max: %hu | size: %zu\n", 
+           (unsigned char) UCHAR_MAX, 
+           sizeof(unsigned char));
+}
+
+/** TODO TASK 4:
+    * Create your stype_sizes function here to print min/max values and sizes of 
+    * signed integer types: char, short, int, long
+    */
